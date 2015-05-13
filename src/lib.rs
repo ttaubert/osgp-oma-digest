32cf743c9533cc19ded4a78780535e37a0c3cc4e
Tim Taubert <tim@timtaubert.de>
2015-05-13 14:16:13 +0200

    refactored process_block() to make the algorithm a tad easier to understand

--- src/lib.rs
+++ src/lib.rs
@@ -18,30 +18,28 @@ impl OMADigest for [u8] {
     let mut state = [0u8; 8];
 
     fn process_block(state: &mut [u8], key: &[u8], block: &[u8]) {
-      // 18 rounds.
-      for i in 0..18 {
-        let key_byte = key[i % KEY_SIZE];
+      // For each byte in the block...
+      for l in 0..144 {
+        // Current position in the state, starts from the back.
+        let j = (7u8.wrapping_sub(l as u8) % 8) as usize;
 
-        // Transform each state byte.
-        for j in (0..8).rev() {
-          let key_bit = key_byte >> (7 - j);
+        // The current key bit to work with.
+        let key_bit = key[(l / 8) % KEY_SIZE] >> 7 - j;
 
-          let index = 8 * i + 7 - j;
-          // The block byte at the given index or 0 if the block is too
-          // short. This basically implements zero-padding short blocks.
-          let block_byte = if index < block.len() { block[index] } else { 0 };
+        // The block byte at the given index or 0 if the block is too
+        // short. This basically implements zero-padding short blocks.
+        let block_byte = if l < block.len() { block[l] } else { 0 };
 
-          // Temp values shared between branches.
-          let yz = state[(j + 1) % 8].wrapping_add(block_byte);
-          let xc = !state[j].wrapping_add(j as u8);
+        // Temp values shared between branches.
+        let yz = state[(j + 1) % 8].wrapping_add(block_byte);
+        let xc = !state[j].wrapping_add(j as u8);
 
-          // Switch based on key bit.
-          state[j] = if key_bit % 2 == 1 {
-            yz.wrapping_add(xc).rotate_left(1)
-          } else {
-            yz.wrapping_sub(xc).rotate_right(1)
-          };
-        }
+        // Switch based on key bit.
+        state[j] = if key_bit % 2 == 1 {
+          yz.wrapping_add(xc).rotate_left(1)
+        } else {
+          yz.wrapping_sub(xc).rotate_right(1)
+        };
       }
     }
 
