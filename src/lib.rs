a7a1eb48be5fdcf38b6357de4937fe8c43cddaf1
Tim Taubert <tim@timtaubert.de>
2015-06-17 21:04:57 -0700

    update for blog post / more clarity

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,36 @@
 const KEY_SIZE: usize = 12;
 const BLOCK_SIZE: usize = 144;
 
+fn inner(state: [u8; 8], key: &[u8], block: &[u8]) -> [u8; 8] {
+  let mut state = state;
+
+  // For each byte in the block...
+  for l in 0..144 {
+    // Current position in the state, starts from the back.
+    let j = (7u8.wrapping_sub(l as u8) % 8) as usize;
+
+    // The current key bit to work with.
+    let key_bit = key[(l / 8) % KEY_SIZE] >> (7 - j);
+
+    // The block byte at the given index or 0 if the block is too
+    // short. This basically implements zero-padding short blocks.
+    let block_byte = if l < block.len() { block[l] } else { 0 };
+
+    // Temp values shared between branches.
+    let yz = state[(j + 1) % 8].wrapping_add(block_byte);
+    let xj = !state[j].wrapping_add(j as u8);
+
+    // Switch based on key bit.
+    state[j] = if key_bit & 1 == 1 {
+      yz.wrapping_add(xj.rotate_left(1))
+    } else {
+      yz.wrapping_sub(xj.rotate_right(1))
+    };
+  }
+
+  state
+}
+
 pub trait OMADigest {
   fn oma_digest(&self, key: &[u8]) -> [u8; 8];
 }
@@ -14,41 +44,10 @@ impl OMADigest for [u8] {
     // Key must be 96 bits.
     assert_eq!(key.len(), KEY_SIZE);
 
-    // The internal state.
-    let mut state = [0u8; 8];
-
-    fn process_block(state: &mut [u8], key: &[u8], block: &[u8]) {
-      // For each byte in the block...
-      for l in 0..144 {
-        // Current position in the state, starts from the back.
-        let j = (7u8.wrapping_sub(l as u8) % 8) as usize;
-
-        // The current key bit to work with.
-        let key_bit = key[(l / 8) % KEY_SIZE] >> (7 - j);
-
-        // The block byte at the given index or 0 if the block is too
-        // short. This basically implements zero-padding short blocks.
-        let block_byte = if l < block.len() { block[l] } else { 0 };
-
-        // Temp values shared between branches.
-        let yz = state[(j + 1) % 8].wrapping_add(block_byte);
-        let xc = !state[j].wrapping_add(j as u8);
-
-        // Switch based on key bit.
-        state[j] = if key_bit & 1 == 1 {
-          yz.wrapping_add(xc.rotate_left(1))
-        } else {
-          yz.wrapping_sub(xc.rotate_right(1))
-        };
-      }
-    }
-
     // Process each block, carrying over state.
-    for block in self.chunks(BLOCK_SIZE) {
-      process_block(&mut state, key, block);
-    }
-
-    state // The final state.
+    self.chunks(BLOCK_SIZE).fold([0u8; 8], |state, block| {
+      inner(state, key, block)
+    })
   }
 }
 
